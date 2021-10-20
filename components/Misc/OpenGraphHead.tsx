3b35a975483e7a3481164e1ec9d1200eb9e29657
HoangSon <sonlth@neoconsulting.com.vn>
2021-10-20 17:16:38 +0700

    init header info

--- components/Misc/OpenGraphHead.tsx
+++ components/Misc/OpenGraphHead.tsx
@@ -2,18 +2,19 @@ import Head from 'next/head'
 const OpenGraphHead = () => (
   <Head>
     <title>Hoang Son | Mobile Developer</title>
+    <meta name="author" content="Le Tran Hoang Son" />
     <meta name="description" content="My personal solace place on web-earth." />
     <meta property="og:title" content="Hoang Son | Mobile Developer" />
-    <meta property="og:site_name" content="Le Tran Hoang Son" />
-    <meta property="og:url" content="https://kllawingco.netlify.app/" />
+    <meta property="og:site_name" content="Hoang Son's Portfolio" />
+    <meta property="og:image" content="/card.png" />
     <meta
       property="og:description"
-      content="Ohh you found me?. Howdy! I am Le Tran Hoang Son. Or you could call me Daniel. That works too . . . I am a Mobile Developer"
+      content="Howdy! I am Hoang Son. Or you could call me Daniel. I am a Mobile Developer"
     />
     <meta property="og:type" content="profile" />
     <meta
       property="og:image"
-      content="https://kllawingco.netlify.app/hs_avatar.png"
+      content="/hs_avatar.png"
     ></meta>
   </Head>
 )
